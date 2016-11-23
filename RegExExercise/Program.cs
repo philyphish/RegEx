d725e8d44253e7315f87bf53c8db613a6fd7afad
UWB <UWB@UWB-EMILY>
2016-11-23 14:45:19 -0500

    Try to get in between string

--- RegExExercise/Program.cs
+++ RegExExercise/Program.cs
@@ -11,12 +11,20 @@ namespace RegExExercise
     {
         static void Main(string[] args)
         {
-            var link = "dfsdkjfskjdfnskjdfsdf<a href='https://www.google.com'/>dmflkmklsmddsc";
-            var pattern = "(<a href='https://\\.?)"; //Matches each occurance of <a href='https://
+            var link = "dfsdkjfskjdfnskjdfsdf<a href='https://www.google.com'/>dmflkmklsmddsc <a href='http://www.yahoo.com'/>";
+            var beginPattern = "<a href=";
+            var endPattern = "/>";
+            var options  = RegexOptions.IgnoreCase;
+            //var pattern = "(<a href='//\\.?)"; //Matches each occurance of <a href='https://
             //Replaces the pattern in link with an empty string
-            var myRegEx = Regex.Replace(link, pattern, String.Empty );
+            //var myRegEx = Regex.Replace(link, pattern, String.Empty );
+            var myRegExMatches = Regex.Matches(link, beginPattern, options);
 
-            Console.WriteLine(myRegEx);
+            foreach (var n in myRegExMatches)
+            {
+                Console.WriteLine(n);
+            }
+            //Console.WriteLine(myRegExMatches.Count);
         }
     }
 }
