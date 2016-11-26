9c7c928b7c139aaac400b59984fb760725a70811
philyphish <philipknigh@gmail.com>
2016-11-26 08:50:01 -0500

    Finding links in a file

--- RegExExercise/Program.cs
+++ RegExExercise/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RegExExercise
 {
@@ -11,20 +12,30 @@ namespace RegExExercise
     {
         static void Main(string[] args)
         {
-            var link = "dfsdkjfskjdfnskjdfsdf<a href='https://www.google.com'/>dmflkmklsmddsc <a href='http://www.yahoo.com'/>";
-            var beginPattern = "<a href=";
-            var endPattern = "/>";
+            var text = File.ReadAllText(@"c:\dev\test.html");
+            var separtingChars = ' ';
+            var links = text.Split(separtingChars, (char)StringSplitOptions.RemoveEmptyEntries );
+            
+            var beginPattern = @"(href)";       // not a word a href @"([^\w*]a href)"
             var options  = RegexOptions.IgnoreCase;
             //var pattern = "(<a href='//\\.?)"; //Matches each occurance of <a href='https://
             //Replaces the pattern in link with an empty string
             //var myRegEx = Regex.Replace(link, pattern, String.Empty );
-            var myRegExMatches = Regex.Matches(link, beginPattern, options);
+            //var myRegExMatches = Regex.Matches(link, beginPattern, options);
 
-            foreach (var n in myRegExMatches)
+            //Console.WriteLine(text);
+            //Regex.Matches(text, beginPattern, options);
+
+            MatchCollection match =  Regex.Matches(text, beginPattern, options);
+            //Console.WriteLine(match);
+            
+
+            foreach (var n in match)
             {
                 Console.WriteLine(n);
             }
-            //Console.WriteLine(myRegExMatches.Count);
+
+            Console.WriteLine(match.Count);
         }
     }
 }
